759b394b5ad3a4218818f37c681d26bd5ada72a3
Alexia-Crisan <150911612+Alexia-Crisan@users.noreply.github.com>
2026-03-05 18:39:20 +0200

    user add to cart

--- JS/add_to_cart.js
+++ JS/add_to_cart.js
@@ -1,14 +1,23 @@
+import { auth } from "./firebase.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
+
 document.addEventListener("DOMContentLoaded", function () {
-    let cart = JSON.parse(localStorage.getItem("cart")) || [];
-  
-    function addToCart(event) {
-      const button = event.target;
-      const productName = button.getAttribute("data-name");
-      const productPrice = parseFloat(button.getAttribute("data-price"));
-      const productImage = button.getAttribute("data-image");
-  
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+  function addToCart(event) {
+    const button = event.target;
+    const productName  = button.getAttribute("data-name");
+    const productPrice = parseFloat(button.getAttribute("data-price"));
+    const productImage = button.getAttribute("data-image");
+
+    onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        alert("Please log in to add items to your cart.");
+        return;
+      }
+
       const existingProduct = cart.find((item) => item.name === productName);
-  
+
       if (existingProduct) {
         existingProduct.quantity += 1;
       } else {
@@ -19,13 +28,13 @@ document.addEventListener("DOMContentLoaded", function () {
           image: productImage,
         });
       }
-  
+
       localStorage.setItem("cart", JSON.stringify(cart));
       alert(`${productName} was added to the cart!`);
-    }
-  
-    document.querySelectorAll(".add-to-cart").forEach((button) => {
-      button.addEventListener("click", addToCart);
     });
+  }
+
+  document.querySelectorAll(".add-to-cart").forEach((button) => {
+    button.addEventListener("click", addToCart);
   });
-  
+});
